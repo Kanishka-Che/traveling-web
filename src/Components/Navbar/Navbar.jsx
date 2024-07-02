01edb7b5ec09874f4d16df0f62aa3a4df928e35d
Kanishka99 <akcadikari@sttd.appsc.sab.ac.lk>
2024-07-02 20:35:35 +0530

    first navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './navbar.css'
-
+import { FaRegWindowClose } from "react-icons/fa";
+import { MdTravelExplore } from "react-icons/md";
 
 const Navbar = () =>{
 
@@ -9,10 +10,15 @@ const Navbar = () =>{
            <header className='header flex'>
 
               
-              <div className="navbar">
-                <ul className="navlists flex">
+              <div className="logoDiv">
+                <a href="#" className='logo flex'>
+                  <h1><MdTravelExplore className='icon'/>TRAVEL</h1>
+                </a>
+              </div>
 
-                    <li className='navItem'>
+                <div>
+                 <ul>
+                    <li>
                       <a href="#" className="navLink">Home</a>
                     </li>
 
@@ -43,8 +49,15 @@ const Navbar = () =>{
                     <button className='btn'>
                         <a href='#'>BOOK NOW </a>
                     </button>
-                </ul>    
-              </div>
+                  </ul>  
+
+                </div>
+
+
+              <div className='closeNavbar'>
+                   <FaRegWindowClose className="icon"/>
+              </div>   
+              
         </header>
         </section>
     )
